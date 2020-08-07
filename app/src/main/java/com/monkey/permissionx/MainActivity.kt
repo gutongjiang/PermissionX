0811ac45a81f4d9fd2ca29824b0df7ccddd0cb7c
chenfei <chenfei0929@gmail.com>
2020-08-07 10:24:06 +0800

    test library

--- app/src/main/java/com/monkey/permissionx/MainActivity.kt
+++ app/src/main/java/com/monkey/permissionx/MainActivity.kt
@@ -1,13 +1,28 @@
 package com.monkey.permissionx
 
+import android.Manifest
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.permissionx.monkey.PermissionX
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        PermissionX.request(
+            this,
+            Manifest.permission.CALL_PHONE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+        ) { allGranted, deniedList ->
+            if (allGranted) {
+                Toast.makeText(this, "权限申请成功", Toast.LENGTH_SHORT).show()
+            } else {
+                Toast.makeText(this, "申请失败 $deniedList", Toast.LENGTH_SHORT).show()
+            }
+        }
     }
 
 }
